6289bb2bae02046219ec7d3159d9772bcb3b4054
Fulano de Tal <joaoofficialpedro@gmail.com>
2023-05-31 23:20:57 -0300

    Atualização Vento Fresco2

--- DataBase/ScriptBD.sql
+++ DataBase/ScriptBD.sql
@@ -4,28 +4,30 @@ USE ventofresco;
 
 
 
-CREATE TABLE `clients` (
+CREATE TABLE `client` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(150) NOT NULL,
     `email` VARCHAR(250) NOT NULL,
     `password` VARCHAR(45) NOT NULL,
     `cell_phone` INT(11) DEFAULT NULL,
     `adress_id_fk` INT DEFAULT NULL,
-    `cart` VARCHAR(100),
-    CONSTRAINT `fk_adress_user`
-	FOREIGN KEY (`adress_id_fk`) REFERENCES `adress_user` (`id`)
+    `cart` VARCHAR(255) DEFAULT '',
+    CONSTRAINT `adress_id_fk`
+	FOREIGN KEY (`adress_id_fk`) REFERENCES `adress_client` (`id`)
 );
-SELECT * FROM `clients`;
+SELECT * FROM `client`;
+DROP TABLE `client`;
 
 
-CREATE TABLE `adresadress_useradress_users_user`(
+
+CREATE TABLE `adress_client`(
 	`id` INT AUTO_INCREMENT PRIMARY KEY,
-    `address` VARCHAR(80),
-    `neighborhood` VARCHAR(30),
-    `city` VARCHAR(30),
-    `state` VARCHAR(2),
-    `number` INT(5)
+    `address` VARCHAR(80) DEFAULT '',
+    `neighborhood` VARCHAR(30) DEFAULT '',
+    `city` VARCHAR(30) DEFAULT '',
+    `state` VARCHAR(2) DEFAULT '',
+    `number` INT(6)
 );
-SELECT * FROM `adress_user`;
-
+SELECT * FROM `adress_client`;
+DROP TABLE `adress_client`;
 
